427e60d3d796551ec18ef78f94681f2133ddf5d6
Charan Teja Lellaboyena <teja.charan20@gmail.com>
2017-11-11 00:37:40 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -12,10 +12,9 @@ def won?(board)
       return win_combination
     elsif board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O"
       return win_combination
-    else
-      return false
     end
   end
+  return false
 end
 
 a = won?(["O", "O", " ", "X", "X", "X", " ", " ", " "])
